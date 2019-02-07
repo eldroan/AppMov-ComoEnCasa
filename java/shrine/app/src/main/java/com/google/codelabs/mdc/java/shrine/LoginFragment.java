4d9f49d510e371e1e63fa238034d8edfac4a64b5
eldroan <leandroamarillo95@gmail.com>
2019-02-07 14:47:41 -0300

    Se crea la pantalla de login de la app usando md

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -2,14 +2,18 @@ package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Fragment representing the login screen for Shrine.
- */
+
 public class LoginFragment extends Fragment {
 
     @Override
@@ -18,10 +22,56 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.shr_login_fragment, container, false);
 
-        // Snippet from "Navigate to the next Fragment" section goes here.
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        MaterialButton nextButton = view.findViewById(R.id.login_button);
+        MaterialButton registerButton = view.findViewById(R.id.register_button);
 
+        // Set an error if the password is less than 8 characters.
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(getString(R.string.shr_error_password));
+                } else {
+                    passwordTextInput.setError(null); // Clear the error
+                    ((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false); // Navigate to the next Fragment
+                }
+            }
+
+
+        });
+
+        // Limpiar el mensaje de error
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(null); //Clear the error
+                }
+                return false;
+            }
+        });
+
+        passwordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus == false){
+                    if (isPasswordValid(passwordEditText.getText())) {
+                        passwordTextInput.setError(null); //Clear the error
+                    }else{
+                        passwordTextInput.setError(getString(R.string.shr_error_password));
+                    }
+                }
+
+            }
+        });
         return view;
     }
 
-    // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+
+    private boolean isPasswordValid(@Nullable Editable text) {
+        //To-DO
+        return text.length() > 0;
+    }
 }
---
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/RegisterFragment.java
@@ -0,0 +1,23 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RegisterFragment extends Fragment {
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.shr_fragment_register, container, false);
+
+
+        return view;
+    }
+
+}
