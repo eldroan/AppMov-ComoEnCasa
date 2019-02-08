73f5a095c261334c2d23245ac45f84ae9e87c2ae
eldroan <leandroamarillo95@gmail.com>
2019-02-08 11:32:13 -0300

    Se agrega la inicializacion de parse en la application class

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/application/ShrineApplication.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/application/ShrineApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.support.v7.app.AppCompatDelegate;
 
+import com.parse.Parse;
+
 public class ShrineApplication extends Application {
     private static ShrineApplication instance;
     private static Context appContext;
@@ -23,6 +25,13 @@ public class ShrineApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("j9A9UOZTzG4BRYYNb5JcGR5De3pCOQbA8OCMZcQg")
+                .clientKey("0Oa3us8Q0EZulKZuaMN5Vlh1StBDAW1PDcbuVeWP")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
         instance = this;
 
         this.setAppContext(getApplicationContext());
