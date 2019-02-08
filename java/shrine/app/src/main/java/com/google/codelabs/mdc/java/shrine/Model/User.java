6f8a5942a142f7c403edc0f291a285cf1c24d11b
eldroan <leandroamarillo95@gmail.com>
2019-02-08 18:07:26 -0300

    Se agregan las clases del modelo de datos

---
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/Model/User.java
@@ -0,0 +1,11 @@
+package com.google.codelabs.mdc.java.shrine.Model;
+
+import android.graphics.Bitmap;
+
+public class User {
+    public String name;
+    public String surname;
+    public String email;
+    public float score;
+    public Bitmap image;
+}
