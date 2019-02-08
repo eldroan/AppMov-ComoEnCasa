087342a0f77f85a0da8bc6ee08c12e09d771025c
Germán Ignacio Bernal <bernalgermanignacio@gmail.com>
2019-02-08 18:37:19 -0300

    Crea el layout de las filas de las mesas virtuales

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/SeleccionarPlatoFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/SeleccionarPlatoFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -15,13 +16,15 @@ import android.view.ViewGroup;
  */
 public class SeleccionarPlatoFragment extends Fragment {
 
-    @Override
-    public View onCreateView(
+    private Button
 
-    @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.shr_fragment_seleccionar_plato, container, false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
 
         return view;
     }
