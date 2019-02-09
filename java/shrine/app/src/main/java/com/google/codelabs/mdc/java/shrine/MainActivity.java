f4fc2737237ae118520bb839133efd2a62bbe916
eldroan <leandroamarillo95@gmail.com>
2019-02-08 21:20:57 -0300

    Se implementa el boton take picture en la creacion de usuario y lo muestra, todavia falta el resto de la funcionalidad

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
@@ -1,12 +1,27 @@
 package com.google.codelabs.mdc.java.shrine;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity implements NavigationHost {
+import com.google.codelabs.mdc.java.shrine.Model.IReceivePicture;
+import com.google.codelabs.mdc.java.shrine.Model.ITakePicture;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements NavigationHost,ITakePicture {
+    IReceivePicture pictureReceiver;
+    String pathPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,4 +67,43 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
 
         transaction.commit();
     }
+
+    //BEGIN codigo para sacar fotos (Extraido del laboratorio 6)
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",dir);
+
+        pathPhoto = image.getAbsolutePath();
+        return image;
+    }
+    @Override
+    public void takePicture(IReceivePicture receiver) {
+        pictureReceiver = receiver;
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) { }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1337);
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int reqCode,int resCode, Intent data) {
+
+        if (reqCode == 1337 && resCode == RESULT_OK) {
+
+            pictureReceiver.receivePicture(pathPhoto);
+        }
+    }
+    //END codigo para sacar fotos (Extraido del laboratorio 6)
 }
--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/RegisterFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/RegisterFragment.java
@@ -1,6 +1,9 @@
 package com.google.codelabs.mdc.java.shrine;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.button.MaterialButton;
@@ -9,13 +12,29 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.codelabs.mdc.java.shrine.Model.IReceivePicture;
+import com.google.codelabs.mdc.java.shrine.Model.ITakePicture;
+import com.google.codelabs.mdc.java.shrine.Model.User;
+import com.google.codelabs.mdc.java.shrine.Parse.UserDAO;
+import com.parse.ParseException;
 
-public class RegisterFragment extends Fragment {
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class RegisterFragment extends Fragment implements UserDAO.IUserCreation, IReceivePicture, View.OnClickListener {
+    private User newUser;
+    private CircleImageView roundImage;
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -23,7 +42,115 @@ public class RegisterFragment extends Fragment {
 
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        roundImage = view.findViewById(R.id.profile_image);
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        final TextInputLayout passwordTextInputConfirmation = view.findViewById(R.id.password_text_input_confirmation);
+        final TextInputEditText passwordEditTextConfirmation = view.findViewById(R.id.password_edit_text_confirmation);
+        final TextInputEditText nameEditText = view.findViewById(R.id.name_field_text);
+        final TextInputEditText surnameEditText = view.findViewById(R.id.surname_field_text);
+        final TextInputEditText emailEditText = view.findViewById(R.id.email_field_text);
+        MaterialButton takePicture = view.findViewById(R.id.take_picture_button);
+
+        takePicture.setOnClickListener(this);
+
+
+
+
         return view;
     }
 
+    private boolean isPasswordValid(@Nullable Editable text) {
+
+        //Las contraseñas deben ser mayor a 8 caracteres, contener mayusculas, contener minusculas y contener al menos un numero
+
+        if(text == null)
+            return false;
+
+        String t = text.toString();
+
+        if(t.length() < 9)
+            return false;
+        if(t.equals(t.toLowerCase())){
+            //Si el texto es igual al resultado de to lowercase es porque no tiene mayusculas
+            return false;
+        }
+        if(t.equals(t.toUpperCase())){
+            //Si el texto es igual al resultado de to uppercase es porque no tiene minusculas
+            return false;
+        }
+
+        //Esto busca si hay un numero dentro del string, gracias stackoverflow
+        Pattern p = Pattern.compile( "[0-9]" );
+        Matcher m = p.matcher( t );
+
+        if(m.find() == false){
+            // el matcher no encontro nada, no hay numeros en el string
+            return false;
+        }
+
+        //Si llegaste hasta aca es porque esta todo bien
+        return true;
+    }
+
+    @Override
+    public void connectionFailed() {
+
+    }
+
+    @Override
+    public void creationSucceded() {
+
+    }
+
+    @Override
+    public void usernameTaken() {
+
+    }
+
+    @Override
+    public void emailTaken() {
+
+    }
+
+    @Override
+    public void creationFailed(ParseException e) {
+
+    }
+
+    @Override
+    public void receivePicture(String path) {
+        //Aca me llega el path de la imagen que se guardo en el almacenamiento externo
+        File file = new File(path);
+
+        Bitmap imageBitmap = null;
+
+        try {
+            imageBitmap = MediaStore.Images.Media
+                    .getBitmap(getActivity().getApplicationContext().getContentResolver(),
+                            Uri.fromFile(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (imageBitmap != null) {
+            //Hacer algo con este bitmap
+            roundImage.setImageBitmap(imageBitmap);
+
+        }else{
+            Log.d("Register","El bitmap fue null");
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.take_picture_button:
+                ((ITakePicture)this.getActivity()).takePicture(this);
+                break;
+            default:
+            break;
+        }
+    }
 }
