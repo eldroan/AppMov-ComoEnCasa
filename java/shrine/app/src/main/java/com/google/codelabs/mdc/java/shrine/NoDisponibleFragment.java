151b4d1cbecb1d8ffe82be08067890e4ba6f63b6
Germán Ignacio Bernal <bernalgermanignacio@gmail.com>
2019-02-09 10:31:28 -0300

    Se agrega el fragment de pantalla no disponible

---
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/NoDisponibleFragment.java
@@ -0,0 +1,23 @@
+package com.google.codelabs.mdc.java.shrine;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NoDisponibleFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.shr_fragment_no_disponible, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        return view;
+    }
+}
